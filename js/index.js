05256a50a67c71a41cb56342d79411703d0edc18
W84Riko <ymykhalyna@gmail.com>
2019-10-11 16:07:45 +0000

    Check multiple operations in row

--- js/index.js
+++ js/index.js
@@ -10,16 +10,37 @@ function btnClicked(ev) {
 }
 
 document.querySelector('.calc .buttons .eq').addEventListener('click',  () => {
-    if(display.value.search('1/0') != -1) {
+    if(display.value.search('/0') != -1) {
         alert("Error /0 !!!");
     }
-    else if(false) {
+    else if(CheckOperations(display.value)) {
         alert("Error there is 2 or more operations in a row!!!");
     }
     else {
         display.value = eval(display.value)
     }
     });
+
+function CheckOperations(string) {
+    let operations = ['+', '-', '*', '/'];
+    let operationsPositions = [];
+    let searchIndex = 0;
+    for(let i = 0; i < operations.length; i++) {
+        while(string.indexOf(operations[i], searchIndex) != -1) {
+            operationsPositions.push(string.indexOf(operations[i], searchIndex));
+            searchIndex=string.indexOf(operations[i], searchIndex) + 1;
+        }
+        searchIndex = 0;
+    }
+    for(let i = 0; i < operationsPositions.length; i++) {
+        for(let j = i + 1; j < operationsPositions.length; j++) {
+            if(operationsPositions[i]+1 === operationsPositions[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
   document.querySelector('.calc .minusPlus').addEventListener('click' , () => {
     if(display.value[0] === '-') {
